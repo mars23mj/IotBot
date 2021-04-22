8f7991d9b50095a47940ab1c6fa6c17bb8d45f80
Marcial Cruz <mars23mj@Marcials-MacBook-Pro.local>
2021-04-21 22:09:24 -0700

    Initial code added to Express template

--- iotbot/routes/index.js
+++ iotbot/routes/index.js
@@ -1,9 +1,133 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+process.env.PAGE_ACCESS_TOKEN ="EAAEQj9490dIBAP46kXWwA5WW5vlEsWgQzG9ZAImM1I7FoLBKTb179m4KAy39QEYOYrvtEiQvvR0S0vWOhvJHIeok9ZA7pVxcvcx976kaZBdzIn4hI98E7LbUU5B1BuQqudCCkwP0pPdlLosTAvcwXU38QuZBsPmYSEM92OequgZDZD";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Adds support for GET requests to our webhook
+router.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "hello_token_success"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
+
+// Creates the endpoint for our webhook 
+router.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log('Sender PSID: ' + sender_psid);
+    
+    console.log('tiktok A');
+    // Check if the event is a message or postback and
+    // pass the event to the appropriate handler function
+    if (webhook_event.message) {
+      handleMessage(sender_psid, webhook_event.message);        
+    } else if (webhook_event.postback) {
+      handlePostback(sender_psid, webhook_event.postback);
+    }
+    });
+
+    console.log('tiktok B');
+
+    // send back message from Messenger
+    //callSendAPI(sender_psid, webhook_event);
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+  console.log('handleM -1');
+  // Check if the message contains text
+  if (received_message.text) {
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }
+ 
+  // Sends the response message
+  callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+  console.log('handleM -2');
+
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  console.log('SendApi -1');
+
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
 
 module.exports = router;
